89e266c18d5bf530930a253e7bbaedf0ac6166d1
Marcus Norrgård <Max@Marcuss-MBP-2.lan>
2016-10-10 18:24:40 +0300

    User class added and small stuffs in MyUi

---
+++ src/main/java/com/yapss/User.java
@@ -0,0 +1,20 @@
+package com.yapss;
+import java.util.Date;
+/**
+ * Created by Max on 10/10/16.
+ */
+public class User {
+
+    private Long id;
+
+    private String firstName = "";
+
+    private String lastName = "";
+
+    private Date birthDate;
+
+    private String email = "";
+
+
+
+}
