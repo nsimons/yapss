1cca5abd372467e84616af06cbd3b83fde205e7f
nsimons <nsimons@abo.fi>
2016-10-23 13:58:20 +0300

    add gitignore, cleanup shits

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM Question;
-INSERT INTO Question VALUES(1, true, 'Prepare presentation');
-INSERT INTO Question VALUES(2, true, 'Procrastinate');
-INSERT INTO Question VALUES(3, false, 'Have presentation');
